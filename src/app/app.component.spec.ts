f3c4c113a064ce37b395c22c03595c0c026126ba
Scott Cooper <scttcper@gmail.com>
2018-06-22 17:07:28 -0700

    chore: mock button service for test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,8 +2,17 @@ import { APP_BASE_HREF } from '@angular/common';
 import { async, TestBed } from '@angular/core/testing';
 import { RouterModule } from '@angular/router';
 
-import { NtkmeButtonModule } from '../lib/public_api';
+import { of as ObservableOf } from 'rxjs';
+
+import { ButtonService, NtkmeButtonModule } from '../lib/public_api';
 import { AppComponent } from './app.component';
+
+class FakeButtonService {
+  repo(user: string, repo: string) {
+    return ObservableOf({ stargazers_count: 0 });
+  }
+}
+
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -17,6 +26,7 @@ describe('AppComponent', () => {
       ],
       providers: [
         { provide: APP_BASE_HREF, useValue : '/' },
+        { provide: ButtonService, useClass: FakeButtonService },
       ]
     });
     TestBed.compileComponents();
