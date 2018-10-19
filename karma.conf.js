805af1eea1e358786f14fcdf41b640bea167f826
Scott Cooper <scttcper@gmail.com>
2018-10-18 19:29:48 -0700

    fix: update to angular 7, silence install warning

--- karma.conf.js
+++ karma.conf.js
@@ -24,6 +24,7 @@ module.exports = function(config) {
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
+    browserDisconnectTimeout: 10000,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
