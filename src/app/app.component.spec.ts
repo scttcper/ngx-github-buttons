9287b3a52cd2b207b3ea4c883efc83c35a9ef497
Scott Cooper <scttcper@gmail.com>
2019-05-07 09:50:44 -0700

    build: setup circleci and upgrade to angular 8

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,17 +2,8 @@ import { APP_BASE_HREF } from '@angular/common';
 import { async, TestBed } from '@angular/core/testing';
 import { RouterModule } from '@angular/router';
 
-import { of as ObservableOf } from 'rxjs';
-
-import { ButtonService, NtkmeButtonModule } from '../lib/public_api';
+import { NtkmeButtonModule } from '../lib/public_api';
 import { AppComponent } from './app.component';
-
-class FakeButtonService {
-  repo(user: string, repo: string) {
-    return ObservableOf({ stargazers_count: 0 });
-  }
-}
-
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -26,11 +17,11 @@ describe('AppComponent', () => {
       ],
       providers: [
         { provide: APP_BASE_HREF, useValue : '/' },
-        { provide: ButtonService, useClass: FakeButtonService },
       ]
     });
     TestBed.compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
