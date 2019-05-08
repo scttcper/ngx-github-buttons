532357e6e8b24245ce933eecd02ab32f04c70a40
Scott Cooper <scttcper@gmail.com>
2019-05-07 22:59:15 -0700

    remove whatever event listener this is

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,6 @@ if (environment.production) {
   enableProdMode();
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  platformBrowserDynamic().bootstrapModule(AppModule, {
-    preserveWhitespaces: false,
-  }).catch(err => console.log(err));
-});
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch(err => console.error(err));
